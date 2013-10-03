775cf224e005341c54b7698cb0fc0826c51c5a59
Marcio Lopes de Faria <marcitolopes@gmail.com>
2013-10-03 00:05:07 -0300

    Update rspec-set.rb
    Allow use with Mongoid

--- lib/rspec-set.rb
+++ lib/rspec-set.rb
@@ -24,16 +24,13 @@ module RSpec
           before(:each) do
             model = send(variable_name)
 
-            if model.is_a?(ActiveRecord::Base)
-              if model.destroyed?
-                # Relig destroyed model
-                model.class.find(i.id)
-              elsif !model.new_record?
-                # Reload saved model
-                model.reload
-              end
-            else
-              warn "#{variable_name} is a #{model.class} - rspec-set works with ActiveRecord models only"
+            
+            if model.destroyed?
+              # Relig destroyed model
+              model.class.find(i.id)
+            elsif !model.new_record?
+              # Reload saved model
+              model.reload
             end
           end
 
